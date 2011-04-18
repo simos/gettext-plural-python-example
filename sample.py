5b343ad3bc161e8b472bd2924a69db703d66ca92
Simos Xenitellis <simos@gnome.org>
2011-04-18 11:39:58 +0300

    Added both singular and plural example

--- sample.py
+++ sample.py
@@ -9,3 +9,6 @@ _ = gettext.gettext
 print _('This is a translatable string.')
 n = 1
 print gettext.ngettext('%(useful_favorable)s of %(useful_total)s people found this review helpful, including you', '%(useful_favorable)s of %(useful_total)s people found this review helpful, including you', n ) % { 'useful_favorable': 10, 'useful_total': n }
+
+n = 12
+print gettext.ngettext('%(useful_favorable)s of %(useful_total)s people found this review helpful, including you', '%(useful_favorable)s of %(useful_total)s people found this review helpful, including you', n ) % { 'useful_favorable': 10, 'useful_total': n }
